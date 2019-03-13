8a857deb3d37179a1eccd51d2596bb31eca32544
eniomarcelobuzaneli <eniomarcelobuzaneli@localhost>
2019-03-13 11:24:01 -0400

    UPDATE 13/03/2019 - 11:24hs.

--- application/logs/log-2019-02-05.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-DEBUG - 2019-02-05 14:58:41 --> UTF-8 Support Enabled
-DEBUG - 2019-02-05 14:58:41 --> No URI present. Default controller set.
-DEBUG - 2019-02-05 14:58:41 --> Global POST, GET and COOKIE data sanitized
-DEBUG - 2019-02-05 14:58:42 --> Login MX_Controller Initialized
-DEBUG - 2019-02-05 14:58:42 --> File loaded: /Applications/NetMake/v81/wwwroot/codeigniter/bzcontroluser/application/modules/login/views/ajax/ajax-js.php
-DEBUG - 2019-02-05 14:58:42 --> File loaded: /Applications/NetMake/v81/wwwroot/codeigniter/bzcontroluser/application/modules/login/views/vLogin.php
-DEBUG - 2019-02-05 14:58:42 --> Total execution time: 0.4996
